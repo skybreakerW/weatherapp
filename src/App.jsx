fd2888ec424657cdd5f46bd07aa264a2a70591fc
Akash Sharma <skysharma777@gmail.com>
2026-08-12 17:22:23 +0530

    Added API
    Added API to get the latitude and longitude for an entered City

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
 import { useState } from 'react'
+import Section1 from './components/Section1.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState("")
+  
+  const [temp, setTemp] = useState()
+  const [speed, setSpeed] = useState()
+  const [humid, setHumid] = useState()
+ 
+
+  const getCoordinates = async () => {
+    const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json&limit=1`,
+        {
+            headers: {
+                'User-Agent': 'WeatherApp/1.0'
+            }
+        }
+    ); 
+    const data = await response.json();
+    const lati = await data[0].lat;
+    const long = await data[0].lon;
+    const loc = await data[0].display_name;
+    setLocation("")
+    console.log(`${lati}`)
+    console.log(`${long}`)
+    console.log(`${loc}`)
+    
+  }
 
   return (
-    <div className='min-h-screen bg-slate-600'>
+    <div className='min-h-screen bg-slate-600 text-white'>
      
+     <Section1 />
+
+     <div className='h-[20vh] bg-slate-900 flex justify-center items-center gap-10'>
+
+      <input
+       type="search" value={location} placeholder="Search City" onChange={(e) => {
+        setLocation(e.target.value)
+      }} 
+      className='bg-slate-400 w-[40%] h-[30%] rounded-xl text-white outline-none px-8 text-2xl' 
+      />
+
+      <button onClick={getCoordinates} className='bg-slate-800 px-12 py-6 rounded-full text-2xl text-white tracking-widest cursor-pointer'>Search</button>
+     </div>
+
     </div>
   )
 }
---
+++ src/components/Section1.jsx
@@ -0,0 +1,12 @@
+
+
+const Section1 = () => {
+
+  return (
+    <div className='h-[80vh] bg-slate-800 flex justify-center items-center'>
+      <h1 className='text-4xl'>Temp</h1>
+    </div>
+  )
+}
+
+export default Section1
