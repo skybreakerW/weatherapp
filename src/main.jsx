a917bedd0e3dcb34ea8edbeff4d6790c325ba466
Akash Sharma <skysharma777@gmail.com>
2026-08-14 01:09:17 +0530

    Wrapped with GeoData
    Added Data Centralisation feature and wrapped the whole app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import GeoData from './context/DataContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
+  <GeoData>
     <App />
-  
+  </GeoData>
 )
