9d018836eb278b491d140643419a58b86ea9acd0
Akash Sharma <skysharma777@gmail.com>
2026-08-14 01:10:30 +0530

    Added Centralised Data

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,18 +1,22 @@
 import { Wind, Droplet, MapPin } from "lucide-react"
-
+import { useContext } from "react"
+import { GeoDataContext } from "../context/DataContext"
 
 const Section1 = (props) => {
+
+  const {temp, speed, humid, here, location} = useContext(GeoDataContext)
+
 
   return (
     <div className='h-[80vh] bg-slate-800 flex flex-col justify-center items-center'>
 
-      <h1 className='text-8xl p-8'>{props.temp}°C</h1>
+      <h1 className='text-8xl p-8'>{temp}°C</h1>
       
-        <h3 className="flex flex-row gap-2 items-center text-lg italic p-1">{props.speed} Km/hr
+        <h3 className="flex flex-row gap-2 items-center text-lg italic p-1">{speed} Km/hr
           <Wind />
         </h3>
 
-      <h3 className="flex flex-row gap-2 items-center text-lg italic p-1">{props.humid}% Humid
+      <h3 className="flex flex-row gap-2 items-center text-lg italic p-1">{humid}% Humid
         <Droplet size={20}/>
       </h3>
 
@@ -20,7 +24,7 @@ const Section1 = (props) => {
 
       <h3 className="flex flex-row gap-2 items-center text-lg font-semibold p-2">
         <MapPin size={24} color="#ffffff" />
-        {props.loc}
+        {here}
       </h3>
 
     </div>
