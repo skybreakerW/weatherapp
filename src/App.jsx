966fc34f6064de43cff7ec78159f6e655bf03afb
Akash Sharma <skysharma777@gmail.com>
2026-08-14 01:21:15 +0530

    Added useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { useContext, useEffect } from 'react'
 import Section1 from './components/Section1.jsx'
 import Section2 from './components/Section2.jsx'
-
+import { GeoDataContext } from './context/DataContext.jsx'
 
 
 
 function App() {
   
+  const {updateLocation} = useContext(GeoDataContext)
+
+  useEffect(() => {
+    updateLocation("London")
+  },[])
 
   return (
     <div className='min-h-screen bg-slate-600 text-white'>
-     
-      <Section1 />
-      <Section2 />
       
+      <Section1 />
+      
+      <Section2 />
 
     </div>
   )
